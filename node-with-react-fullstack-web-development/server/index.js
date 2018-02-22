63656ea6243927ecf9d485630c6775c3908da056
Robin Yan <robin.yan@jet.com>
2018-02-22 11:15:43 -0500

    Added google oauth

--- node-with-react-fullstack-web-development/server/index.js
+++ node-with-react-fullstack-web-development/server/index.js
@@ -1,10 +1,30 @@
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there'});
-});
+//https://console.developers.google.com
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
-// Deploying using Heroku
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
