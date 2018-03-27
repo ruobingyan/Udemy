dcb86da69d4e4246a84febcf3b3cc952b388b94d
Robin Yan <robin.yan@jet.com>
2018-03-26 22:17:54 -0400

    Refactored promise usage in passport.js

--- node-with-react-fullstack-web-development/server/services/passport.js
+++ node-with-react-fullstack-web-development/server/services/passport.js
@@ -10,10 +10,9 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then(user => {
-      done(null, user);
-    });
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
 passport.use(
@@ -24,20 +23,18 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
-
 
 /* ES2017 - use async and await to process promise
 const fetchAlbums = async () => {
