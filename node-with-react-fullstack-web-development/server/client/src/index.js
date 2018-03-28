bdfa520eb7b97f7df56a380927d6e267025ff7a9
Robin Yan <robin.yan@jet.com>
2018-03-27 23:12:50 -0400

    Added basic boilerplate for react and redux

--- node-with-react-fullstack-web-development/server/client/src/index.js
+++ node-with-react-fullstack-web-development/server/client/src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+
 import App from './components/App';
+
+const store = createStore(()=>[], {}, applyMiddleware);
 
 ReactDOM.render(<App />, document.querySelector('#root'));
