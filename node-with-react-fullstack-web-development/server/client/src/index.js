216938b48a46e90268d7d82bea4f6e3cabae64f2
Robin Yan <robin.yan@jet.com>
2018-03-28 22:04:07 -0400

    Wired authReducer to root reducer

--- node-with-react-fullstack-web-development/server/client/src/index.js
+++ node-with-react-fullstack-web-development/server/client/src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
-import App from './components/App';
+import App from "./components/App";
+import reducers from "./reducers";
 
-const store = createStore(()=>[], {}, applyMiddleware);
+const store = createStore(reducers, {}, applyMiddleware());
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
